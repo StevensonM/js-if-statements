5212be1766275fbde0b0876735e9a43fab83dad8
StevensonM <110733098+StevensonM@users.noreply.github.com>
2022-09-11 10:52:28 -0500

    Add files via upload

---
+++ js-if-statements/app.js
@@ -0,0 +1,11 @@
+
+let score = Math.random();
+if (score < 0.5) {
+    console.log("IT IS UNDER 0.5!!!!!!")
+    console.log(score);
+}
+
+if (score >= 0.5) {
+    console.log("IT IS OVER 0.5!!!!")
+    console.log(score);
+}
